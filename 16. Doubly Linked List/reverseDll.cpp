f63461620b27c32aaa379969f8ea213cba58a47b
Shaikh Taufik Uddin <shaikhtaufiku@gmail.com>
2026-08-21 21:32:35 +0530

    Add files via upload

---
+++ 16. Doubly Linked List/reverseDll.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class node
+{
+public:
+    node *prev;
+    int data;
+    node *next;
+
+public:
+    node(node *previous, int value, node *nextp)
+    {
+        prev = previous;
+        data = value;
+        next = nextp;
+    }
+
+public:
+    node(node *previous, int value)
+    {
+        prev = previous;
+        data = value;
+        next = nullptr;
+    }
+
+public:
+    node(int value, node *nextp)
+    {
+        prev = nullptr;
+        data = value;
+        next = nextp;
+    }
+
+public:
+    node(int value)
+    {
+        prev = nullptr;
+        data = value;
+        next = nullptr;
+    }
+};
+class dll
+{
+public:
+    node *create(vector<int> &arr)
+    {
+        int size = arr.size();
+        if (size != 0)
+        {
+            node *head = new node(arr[0]);
+            node *copy = head;
+            for (int i = 1; i < size; i++)
+            {
+                node *temp = new node(arr[i]);
+                temp->prev = copy;
+                copy->next = temp;
+                copy = temp;
+            }
+            return head;
+        }
+        return nullptr;
+    }
+
+public:
+    void print(node *head)
+    {
+        if (head)
+        {
+            while (head)
+            {
+                cout << head->data << " ";
+                head = head->next;
+            }
+            cout << endl;
+            return;
+        }
+        cout << "List doesn't exits" << endl;
+        return;
+    }
+
+public:
+    void reverse(node *&h)
+    {
+        if (!h)
+        {
+            cout << "List doesn't exist";
+            return;
+        }
+        node *curr = h;
+        node *temp = curr;
+        while (temp)
+        {
+            curr = temp;
+            temp = curr->next;
+            curr->next = curr->prev;
+            curr->prev = temp;
+        }
+        h = curr;
+        return;
+    }
+};
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+    dll list;
+    node *head = list.create(arr);
+    list.print(head);
+    list.reverse(head);
+    list.print(head);
+}
