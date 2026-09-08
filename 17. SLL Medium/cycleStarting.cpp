04f296b67858328d1b5f7f1e064cffef041b2f1f
Shaikh Taufik Uddin <shaikhtaufiku@gmail.com>
2026-09-08 22:04:33 +0530

    Add files via upload

---
+++ 17. SLL Medium/cycleStarting.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *ptr;
+
+public:
+    node(int newdata, node *newptr)
+    {
+        data = newdata;
+        ptr = newptr;
+    }
+
+public:
+    node(int dataonly)
+    {
+        data = dataonly;
+        ptr = nullptr;
+    }
+};
+class sll
+{
+public:
+    node *create(int arr[], int size)
+    {
+        node *h = new node(arr[0]);
+        node *m = h;
+        for (int i = 1; i < size; i++)
+        {
+            node *temp = new node(arr[i]);
+            m->ptr = temp;
+            m = temp;
+        }
+        return h;
+    }
+
+public:
+    void print(node *head)
+    {
+        if (!head)
+        {
+            cout << "List doesn't exist" << endl;
+            return;
+        }
+        while (head)
+        {
+            cout << head->data << " ";
+            head = head->ptr;
+        }
+        cout << endl;
+        return;
+    }
+
+public:
+    void cycle(node *head)
+    {
+        node *fast = head;
+        node *slow = head;
+        while (fast && fast->ptr)
+        {
+            slow = slow->ptr;
+            fast = fast->ptr->ptr;
+            if (fast == slow)
+            {
+                slow = head;
+                while (fast != slow)
+                {
+                    fast = fast->ptr;
+                    slow = slow->ptr;
+                }
+                cout << fast->data;
+                return;
+            }
+        }
+        cout << "false";
+        return;
+    }
+};
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    sll list;
+    node *head = list.create(arr, 6);
+    list.print(head);
+    list.cycle(head);
+    return 0;
+}
